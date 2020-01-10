b8b83596c8c5b9e15b34398974d656d7de50a218
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-01-10 13:41:20 +0900

    Add SignUp component

--- README.md
+++ README.md
@@ -24,3 +24,32 @@
 `componentDidMount()`でのマウント時しか fetch を発動しないと、後のユーザーが新しくサインインしたり、サインアウトしたことに気づけない。
 
 `onAuthStateChanged`は常にオープンで、サインインしたか、サインアウトしたかなどを察知する。ずっとオープンだからこそ、unmount した時にはクローズさせなければ memory leak が発生してしまう。
+
+3. Auth Library にユーザー情報が入っているだけなので、firestore に格納する場合は下記。
+
+```javascript
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error", error.message);
+    }
+  }
+  return userRef;
+};
+```
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,12 +18,48 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+/****
+ *
+ * Google Authentication
+ *
+ * * */
 const provider = new firebase.auth.GoogleAuthProvider();
 // custom parameter
-// provider.setCustomParameters({ propmpt: "select_account" });
+provider.setCustomParameters({ propmpt: "select_account" });
 
 // Google Authentication
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+/****
+ *
+ * Firestore
+ *
+ * * */
+// Auth Data into Firestore
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error", error.message);
+    }
+  }
+  return userRef;
+};
 
 // In Case You want whole the library
 export default firebase;
